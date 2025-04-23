4c25c2f6bd3fe6b4ef4de897cd5cb71b9b98a5ef
Gasparin <51677665+SoundSoul1@users.noreply.github.com>
2025-04-23 09:36:27 -0400

    Add files via upload

---
+++ rnadomNemberGenerator/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import random
+
+# Generate a random integer between a specified range (inclusive)
+random_number1 = random.randint(0, 70)
+random_number2 = random.randint(0, 70)
+random_number3 = random.randint(0, 70)
+random_number4 = random.randint(0, 70)
+random_number5 = random.randint(0, 70)
+random_number_powerball = random.randint(0, 25)
+print("Winning Numbers:", random_number1, random_number2, random_number3, random_number4, random_number5)
+print("Power Ball:", random_number_powerball)
+
